ff707e5a40e5f5a9ad03e3313858c4b849e46a57
fpledileep <dileep@flpletech.in>
2026-04-20 11:34:53 +0530

    fix db connection

--- config/Database.php
+++ config/Database.php
@@ -7,10 +7,19 @@ class Database {
     private function __construct()
     {
         try {
+            $host = getenv('MYSQLHOST');
+            $port = getenv('MYSQLPORT');
+            $dbname = getenv('MYSQLDATABASE');
+            $user = getenv('MYSQLUSER');
+            $password = getenv('MYSQLPASSWORD');
+
+            if(!$host || !$port || !$dbname || !$user || !$password) {
+                throw new Exception("Database configuration is incomplete");
+            }
             $this->conn = new PDO(
-                "mysql:host=" . getenv('MYSQLHOST') . ";port=" . getenv('MYSQLPORT') . ";dbname=" . getenv('MYSQLDATABASE') . ";charset=utf8mb4",
-                getenv('MYSQLUSER'),
-                getenv('MYSQLPASSWORD'),
+                "mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname . ";charset=utf8mb4",
+                $user,
+                $password,
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
