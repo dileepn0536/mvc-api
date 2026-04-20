fb9e3c626a99759b0ca3210eb0c45649aa1b4d33
dileepnadukuru <36154349+dileepn0536@users.noreply.github.com>
2026-04-20 06:28:33 +0530

    Update Router.php

--- core/Router.php
+++ core/Router.php
@@ -6,7 +6,8 @@ class Router
         $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $url = trim($url, '/');
         $url = strtolower($url);
-        $url = str_replace('oops/mvc', '', $url);
+        // $url = str_replace('oops/mvc', '', $url);
+        echo $url;die;
 
         $method = $_SERVER['REQUEST_METHOD'];
 
