f017b250b5956f1ff354f0bb35e7ce83a4f1d43d
fpledileep <dileep@flpletech.in>
2026-05-03 13:07:46 +0530

    add env file condition

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+if(file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+}
 
 require_once __DIR__ . '/../src/Core/helpers.php';
 
